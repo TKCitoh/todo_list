63b8148c921e5c4fa0a951043b9b2a9c6af55aaf
TakashiIto <tkci10.pooh@gmail.com>
2024-05-05 01:17:44 +0900

    [add]タスクテーブル追加

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController; //追記必要
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/list', [TodoListController::class, 'index']);
+Route::resource('tasks', TaskController::class);
