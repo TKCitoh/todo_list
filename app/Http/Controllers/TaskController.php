8c36cefc5e7bc56eb6052dfac8f5a647493521d9
TakashiIto <tkci10.pooh@gmail.com>
2024-05-08 00:11:39 +0900

    [add]削除機能追加

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,8 +13,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
-  
+        $tasks = Task::where('status', false)->get();
+ 
         return view('tasks.index', compact('tasks'));
     }
 
@@ -75,33 +75,47 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $rules = [
+        //「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
             'task_name' => 'required|max:100',
-          ];
+            ];
         
-          $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
         
-          Validator::make($request->all(), $rules, $messages)->validate();
+            Validator::make($request->all(), $rules, $messages)->validate();
         
         
-          //該当のタスクを検索
-          $task = Task::find($id);
+            //該当のタスクを検索
+            $task = Task::find($id);
         
-          //モデル->カラム名 = 値 で、データを割り当てる
-          $task->name = $request->input('task_name');
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
         
-          //データベースに保存
-          $task->save();
+            //データベースに保存
+            $task->save();
+        } else {
+            //「完了」ボタンを押したとき
         
-          //リダイレクト
-          return redirect('/tasks');
+            //該当のタスクを検索
+            $task = Task::find($id);
+        
+            //モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; //true:完了、false:未完了
+        
+            //データベースに保存
+            $task->save();
+        }
+        
+        
+        //リダイレクト
+        return redirect('/tasks');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        Task::find($id)->delete();
+  
+        return redirect('/tasks');
     }
 }
