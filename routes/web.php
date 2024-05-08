cc6c1221c23bf0bb51dca871fba7cac04bab2681
TakashiIto <tkci10.pooh@gmail.com>
2024-05-09 00:32:17 +0900

    [add]Laravel Breeze追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoListController; //追記必要
 use App\Http\Controllers\TaskController;
@@ -10,3 +11,19 @@ Route::get('/', function () {
 
 Route::get('/list', [TodoListController::class, 'index']);
 Route::resource('tasks', TaskController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/only-verified', function () {
+    return view('only-verified');
+ })->middleware(['auth', 'verified']);
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-      "./resources/views/todo_list/index.blade.php",
-      "./resources/views/tasks/index.blade.php",
-      "./resources/views/tasks/edit.blade.php",
-  ],
-  theme: {
-      extend: {},
-  },
-  plugins: [],
-}
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
 
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
